fb213bbb22173602a3150ff6878cb1ff88f01afc
nnydjesus <nnydjesus@gmail.com>
2016-05-01 03:57:56 -0300

    Actualizacion del README

--- src/main/scala/ar.edu.unq.lids.arq2/persistence/Repository.scala
+++ src/main/scala/ar.edu.unq.lids.arq2/persistence/Repository.scala
@@ -5,7 +5,7 @@ import ar.edu.unq.lids.arq2.configuration
 import ar.edu.unq.lids.arq2.controllers.{Paging, ListResult}
 import ar.edu.unq.lids.arq2.service.Resource
 import net.fwbrasil.activate.statement.query.{Query, PaginationNavigator}
-import net.fwbrasil.activate.statement.{Where, Criteria, StatementSelectValue}
+import net.fwbrasil.activate.statement.{BooleanOperatorCriteria, Where, Criteria, StatementSelectValue}
 
 class Repository[T<:Resource](implicit m: Manifest[T]) {
 
@@ -22,7 +22,7 @@ class Repository[T<:Resource](implicit m: Manifest[T]) {
   }
 
   def filter(filters:List[((T)=>String, Option[String])], limit:Option[Int]=None, offset:Option[Int]=None):QueryResult[T] = {
-    val queryLimit = limit.getOrElse(configuration.server.listPageSize)
+    val queryLimit = limit.getOrElse(configuration.response.limitSize).max(configuration.response.maxLimitSize)
     val queryOffset = offset.getOrElse(0)
 
     val baseQuery = (t:T) => Where(filter(t, filters)) select(t) orderBy(t.id)
@@ -40,10 +40,24 @@ class Repository[T<:Resource](implicit m: Manifest[T]) {
     if(criterias.isEmpty) {
       None
     }else {
-      Some(criterias.reduce[Criteria]((c1, c2) => (c1 :&& c2).asInstanceOf[Criteria]))
+      Some(criterias.reduce[Criteria]((c1, c2) => (c1 :&&c2).asInstanceOf[Criteria]))
     }
   }
+
 }
+
+
+trait FilterOperator{
+  def apply(c1:Criteria, c2:Criteria):BooleanOperatorCriteria
+}
+
+object AndOperator extends FilterOperator{
+  def apply(c1:Criteria, c2:Criteria) = c1 :&& c2
+}
+object OrOperator extends  FilterOperator{
+  def apply(c1:Criteria, c2:Criteria) = c1 :|| c2
+}
+
 
 
 case class QueryResult[T](items:List[T], limit:Int, offset:Int, total:Int)
