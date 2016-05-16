147eefc0edee9924b46df37e43bedb68db2cd0dc
susanrosito <rosito.susana@gmail.com>
2016-05-16 16:23:02 -0300

    Arreglando errores en los archivos .sh para docker. Acomodando acomodando el codigo en los Controllers.

--- build_image.sh
+++ build_image.sh
@@ -1,26 +1,23 @@
+#!/bin/bash
 image=$1
 
-function buildCareprices(){
+function buildCareprices {
     echo -e "\e[01;33m!!!!!!!!!!!!!!!!!!!!   Building Careprices   !!!!!!!!!!!!!!!!!!!!!\e[00m"
     docker-compose -f docker/docker-compose.yml build
 }
-
-function buildMongo(){
+function buildMongo {
     echo -e "\e[01;33m!!!!!!!!!!!!!!!!!!!!   Building mongo   !!!!!!!!!!!!!!!!!!!!!\e[00m"
     docker pull mongo
 }
-
-function buildMysql(){
+function buildMysql {
     echo -e "\e[01;33m!!!!!!!!!!!!!!!!!!!!   Building Mysql   !!!!!!!!!!!!!!!!!!!!!\e[00m"
     docker pull mysql
 }
-
-function buildRedis(){
+function buildRedis {
     echo -e "\e[01;33m!!!!!!!!!!!!!!!!!!!!   Building Redis   !!!!!!!!!!!!!!!!!!!!!\e[00m"
     docker pull redis
 }
-
-function showErrorImage(){
+function showErrorImage {
     echo -e "\e[01;33m!!!!!!!!!!!!!!!!!!!!  Error en el parametro  !!!!!!!!!!!!!!!!!!!!!\e[00m"
     echo "Las posibles opciones:"
 	echo "  * careprices"
@@ -30,7 +27,6 @@ function showErrorImage(){
 	echo ""
 	exit 1
 }
-
 case $image in
   "careprices") buildCareprices ;;
   "mongo") buildMongo ;;
--- src/main/scala/ar.edu.unq.lids.arq2/controllers/Controllers.scala
+++ src/main/scala/ar.edu.unq.lids.arq2/controllers/Controllers.scala
@@ -8,56 +8,55 @@ import com.twitter.finagle.http.{Request, Status}
 import com.twitter.finatra.http.Controller
 
 @Singleton
-class ServerController extends Controller{
-  get("/health") {request: Request =>
+class ServerController extends Controller {
+  get("/health") { request: Request =>
     response.ok.status = Status.Ok
   }
 
-  get("/info") {request: Request =>
+  get("/info") { request: Request =>
     response.ok.json(BuildInfo.toJson)
   }
 
-  get("/config") {request: Request =>
+  get("/config") { request: Request =>
     response.ok.json(ConfigurationResponse())
   }
 
-  get("/") {request: Request =>
+  get("/") { request: Request =>
     response.ok.body("It works!!")
   }
 }
 
 @Singleton
-class ProductController extends ResourceController[Product, ProductDTO]{
+class ProductController extends ResourceController[Product, ProductDTO] {
   get("/products")(all)
   post("/products")(save("products"))
-  get("/products/:id") (byId)
+  get("/products/:id")(byId)
 }
 
 @Singleton
-class PriceController extends ResourceController[Price, PriceDTO]{
+class PriceController extends ResourceController[Price, PriceDTO] {
   override val service = new PriceService
 
   get("/found-prices")(all)
-
   post("/found-prices")(save("found-prices"))
   get("/found-prices/:id")(byId)
 
-  get("/prices/average/:id"){ request: PriceRequest =>
+  get("/prices/average/:id") { request: PriceRequest =>
     response.ok.json(service.average(request).toJson)
   }
 }
 
 @Singleton
-class ShopController extends ResourceController[Shop, ShopDTO]{
+class ShopController extends ResourceController[Shop, ShopDTO] {
   override val service = new ShopService
 
-  get("/shops"){ request: ShopRequest =>
-    response.ok.json(cache(request){service.search(request)})
+  get("/shops") { request: ShopRequest =>
+    response.ok.json(cache(request) {
+      service.search(request)
+    })
   }
-
-  get("/shops/:id")(byId)
-
   post("/shops")(save("shops"))
+  get("/shops/:id")(byId)
 
 }
 
