fd934007aa06692c22d8b5a40664058f469455a7
nnydjesus <nnydjesus@gmail.com>
2016-06-13 15:17:41 -0300

    script para crear cluster de mongo

---
+++ mongo_cluster.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+#sudo route -n add -net 172.17.0.0 192.168.99.100
+#osx
+sudo route -n add -net 172.17.0.0 192.168.99.100
+#linux
+sudo route -net 172.17.0.0 netmask 255.255.0.0 gw 10.2.0.10
+
+docker run -d -p 172.17.0.0:53:53/udp --name skydns crosbymichael/skydns -nameserver 8.8.8.8:53 -domain docker
+docker run -d -v /var/run/docker.sock:/docker.sock --name skydock crosbymichael/skydock -ttl 30 -environment dev -s /docker.sock -domain docker -name skydns
+
+#docker run --name mongo.rs1.0 -h rs1-0.mongo -p 27017 -v /usr/local/mongodb:/data/db -d mongo --replSet rs1 --smallfiles
+
+docker run --name rs1-0.mongo -h rs1-0.mongo --dns 172.17.0.0 -p 27017 -d mongo --replSet rs1 --smallfiles
+docker run --name rs1-1.mongo -h rs1-1.mongo --dns 172.17.0.0 -p 27017 -d mongo --replSet rs1 --smallfiles
+docker run --name rs1-2.mongo -h rs1-2.mongo --dns 172.17.0.0 -p 27017 -d mongo --replSet rs1 --smallfiles
+
+docker exec -it rs1-2.mongo bash
+mongo
+rs.initiate()
+rs.add("rs1-0.mongo:27017")
+rs.add("rs1-1.mongo:27017")
+exit
+exit
+
+docker run --name rs2-0.mongo -h rs2-0.mongo -p 27017 -d mongo --replSet rs2 --smallfiles
+docker run --name rs2-1.mongo -h rs2-1.mongo -p 27017 -d mongo --replSet rs2 --smallfiles
+docker run --name rs2-2.mongo -h rs2-2.mongo -p 27017 -d mongo --replSet rs2 --smallfiles
+
+docker exec -it rs2-2.mongo bash
+mongo
+rs.initiate()
+rs.add("rs2-0.mongo:27017")
+rs.add("rs2-1.mongo:27017")
+exit
+exit
+
+docker run --name rs3-0.mongo  -h  rs3-0.mongo -p 27017 -d mongo --configsvr --replSet rs3 --port 27017
+docker run --name rs3-1.mongo  -h  rs3-1.mongo -p 27017 -d mongo --configsvr --replSet rs3 --port 27017
+
+docker exec -it rs3-0.mongo bash
+mongo
+rs.initiate()
+rs.add("rs3-1.mongo:27017")
+exit
+exit
+
+docker run --name mongo -h  172.17.0.10 -p 27017:27017 -d lgatica/mongos --port 27017 --configdb confRS/172.17.0.8:27017,172.17.0.9:27017
+
+./mongo --host 192.168.99.100:27017
+sh.addShard("rs1/172.17.0.2:27017" )
+sh.addShard("rs2/172.17.0.6:27017" )
+
+docker run --name careprices -h careprices -p 9200:9200  --link mongo  -m 2G -e PORT=9200 -e storage=mongo -d docker_careprices
